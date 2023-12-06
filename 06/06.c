b11ea8aad3cad59587f1d4282dcad4b5a1adca18
Alexander Tarasov <alex.tarasov@sentry.io>
2023-12-06 16:57:39 +0100

    06-1

---
+++ 06/06.c
@@ -0,0 +1,71 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, const char* argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s input.txt [mode]\n", argv[0]);
+    return 1;
+  }
+
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    printf("Error opening file: %s\n", argv[1]);
+    return 2;
+  }
+
+  int mode = 1;
+  if (argc > 2) {
+    mode = atoi(argv[2]);
+  }
+
+  unsigned int times[10];
+  unsigned long long int distances[10];
+
+  int n_races = 0;
+  fscanf(f, "Time:     ");
+  while (1) {
+    int res;
+    res = fscanf(f, "%u", &times[n_races]);
+    if (res != 1) break;
+    n_races++;
+  };
+  n_races = 0;
+  fscanf(f, "Distance: ");
+  while (1) {
+    int res;
+    res = fscanf(f, "%llu", &distances[n_races]);
+    if (res != 1) break;
+    n_races++;
+  };
+
+  unsigned long int result = 1;
+
+  if (mode == 1) {
+    int i;
+    unsigned long long t;
+    for (i=0; i<n_races; i++) {
+      unsigned int number_of_ways_to_win = 0;
+
+      // naively try all times
+      for (t=0; t<=times[i]; t++) {
+        unsigned long long int dist = t * (times[i] - t);
+        if (dist > distances[i]) {
+          number_of_ways_to_win++;
+        }
+      }
+
+      if (number_of_ways_to_win > 0)
+      result *= number_of_ways_to_win;
+    }
+  }
+  else {
+  }
+
+  printf("Answer: %lu\n", result);
+
+  return 0;
+}
