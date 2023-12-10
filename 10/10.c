fcdf536b5ef98beeb33e1a600caad0f14fc14d9e
Alexander Tarasov <alex.tarasov@sentry.io>
2023-12-10 22:43:26 +0100

    10-2 but dirty

--- 10/10.c
+++ 10/10.c
@@ -24,31 +24,32 @@ int main(int argc, const char* argv[]) {
   }
 
   char grid[200][200];
-  // int dist[200][200];
+  bool part_of_loop[200][200];
   int n = 0;
+  int m = 0;
 
-  // char s[200];
   while (!feof(f)) {
     if (fgets(grid[n], 199, f) == NULL) {
         break;
     }
+    m = strlen(grid[n]) - 1;
     n++;
   }
 
-  // for (int i=0; i<n; i++) {
-  //   for (int j=0; j<n; j++) {
-  //     dist[i][j] = -1;
-  //   }
-  // }
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<m; j++) {
+      part_of_loop[i][j] = false;
+    }
+  }
 
   int i_start = 0;
   int j_start = 0;
   for (int i=0; i<n; i++) {
-    for (int j=0; j<n; j++) {
+    for (int j=0; j<m; j++) {
       if (grid[i][j] == 'S') {
         i_start = i;
         j_start = j;
-        // dist[i][j] = 0;
+        part_of_loop[i][j] = true;
       }
     }
   }
@@ -58,19 +59,14 @@ int main(int argc, const char* argv[]) {
   // printf("%c\n", grid[i_start][j_start+1]);
   // printf("%c\n", grid[i_start+1][j_start]);
 
-  int prevdist = 0;
+  int loop_length = 1;
 
-  // example1
+  // example1, example2, example3
   // int i_next = i_start;
   // int j_next = j_start+1;
   // int direction = 2;
 
-  // example2
-  // int i_next = i_start;
-  // int j_next = j_start+1;
-  // int direction = 2;
-
-  // input
+  // input, example4
   int i_next = i_start + 1;
   int j_next = j_start;
   int direction = 3;
@@ -80,80 +76,80 @@ int main(int argc, const char* argv[]) {
     if (grid[i_next][j_next] == 'S') break;
     if (direction == 0) {
       if (grid[i_next][j_next] == '|') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         i_next--;
         direction = 0;
       }
       else if (grid[i_next][j_next] == 'F') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         j_next++;
         direction = 2;
       }
       else if (grid[i_next][j_next] == '7') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         j_next--;
         direction = 1;
       }
     }
     else if (direction == 1) {
       if (grid[i_next][j_next] == '-') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         j_next--;
         direction = 1;
       }
       else if (grid[i_next][j_next] == 'L') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         i_next--;
         direction = 0;
       }
       else if (grid[i_next][j_next] == 'F') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         i_next++;
         direction = 3;
       }
     }
     else if (direction == 2) {
       if (grid[i_next][j_next] == '-') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         j_next++;
         direction = 2;
       }
       else if (grid[i_next][j_next] == '7') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         i_next++;
         direction = 3;
       }
       else if (grid[i_next][j_next] == 'J') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         i_next--;
         direction = 0;
       }
     }
     else if (direction == 3) {
       if (grid[i_next][j_next] == '|') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         i_next++;
         direction = 3;
       }
       else if (grid[i_next][j_next] == 'J') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         j_next--;
         direction = 1;
       }
       else if (grid[i_next][j_next] == 'L') {
-        // dist[i_next][j_next] = prevdist+1;
-        prevdist++;
+        part_of_loop[i_next][j_next] = true;
+        loop_length++;
         j_next++;
         direction = 2;
       }
@@ -161,16 +157,83 @@ int main(int argc, const char* argv[]) {
   }
 
 
-  // output dist map
-  // for (int i=0; i<n; i++) {
-  //   for (int j=0; j<n; j++) {
-  //     printf("%d ", dist[i][j]);
-  //   }
-  //   printf("\n");
-  // }
+  printf("Max distance: %d\n", loop_length/2);
 
-  printf("Loop dist: %d\n", prevdist+1);
-  printf("Answer: %d\n", (prevdist+1)/2);
+  // grid[i_start][j_start] = 'F';  // example1, example2, example3
+  grid[i_start][j_start] = '7';  // input, example4
+
+
+  bool inside = false;
+  int num_inside = 0;
+  char prev_symbol = '.';
+  for (int i=0; i<n; i++) {
+    inside = false;
+    prev_symbol = '.';
+    for (int j=0; j<m; j++) {
+      if (!part_of_loop[i][j]) {
+        if (inside) {
+          num_inside++;
+          // printf("[%d %d]\n", i, j);
+        } else {
+                  prev_symbol = '.';
+        }
+      }
+      if (part_of_loop[i][j]) {
+        char c = grid[i][j];
+
+        if (c == '|') {
+          inside = !inside;
+            // printf("{%d %d} %d\n", i, j, inside);
+            prev_symbol = '.';
+        } else
+        if (c == 'F') {
+          if (prev_symbol == 'J') {
+            inside = !inside;
+            // printf("{%d %d} %d\n", i, j, inside);
+            prev_symbol = '.';
+          } else
+          {
+            prev_symbol = c;
+          }
+        } else if (c=='L') {
+          if (prev_symbol == '7') {
+            inside = !inside;
+            // printf("{%d %d} %d\n", i, j, inside);
+            prev_symbol = '.';
+          }
+          else
+          {
+            prev_symbol = c;
+          }
+        } else if (c=='7') {
+          if (prev_symbol == 'L') {
+            inside = !inside;
+            // printf("{%d %d} %d\n", i, j, inside);
+            prev_symbol = '.';
+          } else if (prev_symbol == 'F') {
+            prev_symbol = '.';
+          }
+          else
+          {
+            prev_symbol = c;
+          }
+        } else         if (c == 'J') {
+          if (prev_symbol == 'F') {
+            inside = !inside;
+            // printf("{%d %d} %d\n", i, j, inside);
+            prev_symbol = '.';
+          } else if (prev_symbol == 'L') {
+            prev_symbol = '.';
+          } else
+          {
+            prev_symbol = c;
+          }
+        }
+      }
+    }
+  }
+
+  printf("Area: %d\n", num_inside);
 
   return 0;
 }
