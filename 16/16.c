058c8a14964dfa61b89317913a8a4caa3efe37f3
Alexander Tarasov <alex.tarasov@sentry.io>
2023-12-16 16:16:51 +0100

    16-2

--- 16/16.c
+++ 16/16.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,7 +16,8 @@ typedef struct beam {
     unsigned char direction;
 } beam;
 
-beam beams[1000];
+#define MAX_BEAMS 2000
+beam beams[MAX_BEAMS];
 unsigned int nbeams;
 
 unsigned char energize[200][200] = {0};
@@ -30,6 +30,97 @@ void print(const unsigned char (*g)[200], unsigned int n, unsigned int m) {
     printf("\n");
   }
   printf("------------------------------------\n");
+}
+
+int solve(const char (*g)[200], int n, int m) {
+  for(int i=0; i<n; i++)
+  for(int j=0; j<m; j++)
+    energize[i][j] = 0;
+
+  for (unsigned int k=0; k<nbeams; k++) {
+    if (nbeams > MAX_BEAMS) {
+      printf("Max number of beams reached!\n");
+      return 0;
+    }
+    beam b = beams[k];
+    int i = b.i;
+    int j = b.j;
+
+    // printf("processing beam [%d,%d,%c]\n", i, j, b.direction+'0');
+    if ( (i<0) || (i>n-1) || (j<0) || (j>m-1) ) continue;
+
+    while (1) {
+      // break if the tile was already energized in that direction
+      if (energize[i][j] & b.direction) break;
+
+      energize[i][j] |= b.direction;
+
+      if ((b.direction & (LEFT|RIGHT)) && (g[i][j] == '|')) {
+        beams[nbeams++] = (beam){i-1, j, UP};
+        beams[nbeams++] = (beam){i+1, j, DOWN};
+        break;
+      } else
+      if ((b.direction & (DOWN|UP)) && (g[i][j] == '-')) {
+        beams[nbeams++] = (beam){i, j-1, LEFT};
+        beams[nbeams++] = (beam){i, j+1, RIGHT};
+        break;
+      } else
+      if ((b.direction == RIGHT) && (g[i][j] == '/')) {
+        beams[nbeams++] = (beam){i-1, j, UP};
+        break;
+      } else
+      if ((b.direction == UP) && (g[i][j] == '/')) {
+        beams[nbeams++] = (beam){i, j+1, RIGHT};
+        break;
+      } else
+      if ((b.direction == LEFT) && (g[i][j] == '/')) {
+        beams[nbeams++] = (beam){i+1, j, DOWN};
+        break;
+      } else
+      if ((b.direction == DOWN) && (g[i][j] == '/')) {
+        beams[nbeams++] = (beam){i, j-1, LEFT};
+        break;
+      } else
+      if ((b.direction == RIGHT) && (g[i][j] == '\\')) {
+        beams[nbeams++] = (beam){i+1, j, DOWN};
+        break;
+      } else
+      if ((b.direction == UP) && (g[i][j] == '\\')) {
+        beams[nbeams++] = (beam){i, j-1, LEFT};
+        break;
+      } else
+      if ((b.direction == LEFT) && (g[i][j] == '\\')) {
+        beams[nbeams++] = (beam){i-1, j, UP};
+        break;
+      } else
+      if ((b.direction == DOWN) && (g[i][j] == '\\')) {
+        beams[nbeams++] = (beam){i, j+1, RIGHT};
+        break;
+      }
+
+      if (b.direction == RIGHT) {
+        j++;
+      }
+      else if (b.direction == DOWN) {
+        i++;
+      }
+      else if (b.direction == LEFT) {
+        j--;
+      }
+      else if (b.direction == UP) {
+        i--;
+      }
+      if ( (i<0) || (i>n-1) || (j<0) || (j>m-1) ) break;
+    }
+  }
+
+  // print(energize, n, m);
+
+  int result = 0;
+  for (int i=0; i<n; i++)
+  for (int j=0; j<m; j++)
+    if (energize[i][j]) result++;
+  return result;
 }
 
 int main(int argc, const char* argv[]) {
@@ -59,93 +150,38 @@ int main(int argc, const char* argv[]) {
     n++;
   }
 
-  nbeams = 1;
-  beams[0] = (beam){0, 0, RIGHT};
+  int result = 0;
+  if (mode == 1) {
+    nbeams = 1;
+    beams[0] = (beam){0, 0, RIGHT};
+    result = solve(grid, n, m);
+  } else {
+    nbeams = 1;
+    int result_candidate;
+    for (int i=0; i<n; i++) {
+      nbeams = 1;
+      beams[0] = (beam){i, 0, RIGHT};
+      result_candidate = solve(grid, n, m);
+      if (result_candidate > result) result = result_candidate;
 
-  for (unsigned int k=0; k<nbeams; k++) {
-    beam b = beams[k];
-    int i = b.i;
-    int j = b.j;
-    printf("processing beam [%d,%d,%c]\n", i, j, b.direction+'0');
-    if (i<0) continue;
-    if (j<0) continue;
-    if (i>n-1) continue;
-    if (j>m-1) continue;
+      nbeams = 1;
+      beams[0] = (beam){i, m-1, LEFT};
+      result_candidate = solve(grid, n, m);
+      if (result_candidate > result) result = result_candidate;
+    }
 
-    while (true) {
-      // break if the tile was already energized in that direction
-      if (energize[i][j] & b.direction) break;
+    for (int j=0; j<m; j++) {
+      nbeams = 1;
+      beams[0] = (beam){0, j, DOWN};
+      result_candidate = solve(grid, n, m);
+      if (result_candidate > result) result = result_candidate;
 
-      energize[i][j] |= b.direction;
-
-      if ((b.direction & (LEFT|RIGHT)) && (grid[i][j] == '|')) {
-        beams[nbeams++] = (beam){i-1, j, UP};
-        beams[nbeams++] = (beam){i+1, j, DOWN};
-        break;
-      } else
-      if ((b.direction & (DOWN|UP)) && (grid[i][j] == '-')) {
-        beams[nbeams++] = (beam){i, j-1, LEFT};
-        beams[nbeams++] = (beam){i, j+1, RIGHT};
-        break;
-      } else
-      if ((b.direction == RIGHT) && (grid[i][j] == '/')) {
-        beams[nbeams++] = (beam){i-1, j, UP};
-        break;
-      } else
-      if ((b.direction == UP) && (grid[i][j] == '/')) {
-        beams[nbeams++] = (beam){i, j+1, RIGHT};
-        break;
-      } else
-      if ((b.direction == LEFT) && (grid[i][j] == '/')) {
-        beams[nbeams++] = (beam){i+1, j, DOWN};
-        break;
-      } else
-      if ((b.direction == DOWN) && (grid[i][j] == '/')) {
-        beams[nbeams++] = (beam){i, j-1, LEFT};
-        break;
-      } else
-      if ((b.direction == RIGHT) && (grid[i][j] == '\\')) {
-        beams[nbeams++] = (beam){i+1, j, DOWN};
-        break;
-      } else
-      if ((b.direction == UP) && (grid[i][j] == '\\')) {
-        beams[nbeams++] = (beam){i, j-1, LEFT};
-        break;
-      } else
-      if ((b.direction == LEFT) && (grid[i][j] == '\\')) {
-        beams[nbeams++] = (beam){i-1, j, UP};
-        break;
-      } else
-      if ((b.direction == DOWN) && (grid[i][j] == '\\')) {
-        beams[nbeams++] = (beam){i, j+1, RIGHT};
-        break;
-      }
-
-      if (b.direction == RIGHT) {
-        j++;
-      }
-      else if (b.direction == DOWN) {
-        i++;
-      }
-      else if (b.direction == LEFT) {
-        j--;
-      }
-      else if (b.direction == UP) {
-        i--;
-      }
-      if (i<0) break;
-      if (j<0) break;
-      if (i>n-1) break;
-      if (j>m-1) break;
+      nbeams = 1;
+      beams[0] = (beam){n-1, j, UP};
+      result_candidate = solve(grid, n, m);
+      if (result_candidate > result) result = result_candidate;
     }
   }
-
-  print(energize, n, m);
-
-  int result = 0;
-  for (int i=0; i<n; i++)
-  for (int j=0; j<m; j++)
-    if (energize[i][j]) result++;
 
   printf("Answer: %d\n", result);
 
